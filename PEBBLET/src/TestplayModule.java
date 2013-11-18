70314146bf9a14afe65def413b1018bf115c2fdc
baek449 <baek449@gmail.com>
2013-11-18 18:12:51 +0900

    20131118b first draft implmentation up to cards

--- PEBBLET/src/TestplayModule.java
+++ PEBBLET/src/TestplayModule.java
@@ -17,10 +17,13 @@ public class TestplayModule {
 	// 변수의 실제 값 목록. 첫 번째 인덱스는 플레이어 번호(0이면 전역), 두 번째 인덱스는 dictionary에서 찾은 변수의 인덱스 값.
 	// 정수, 문자열, 또는 Node (Deck의 경우)를 들고있다.
 	private Object[][] variables;
+	
+	// 예시
+	// global_variable_index : [center:0 discard:1]
+	// player_variable_index : [hand:0 score:1 current:2]
+	// 이 경우, variables에 다음과 같이 저장된다.
+	// [[gl_center gl_discard] [p1_hand p1_score pl_current] [p2_hand p2_score p2_current]...]
 	/////////////////////////////////////////////////////////////
-	
-	private Node def_;
-	
 	
 	private Node def;
 	private Stack<Integer> playerStack;
@@ -30,13 +33,20 @@ public class TestplayModule {
 	private boolean logging;
 	private TestLog testlog;
 	
+	private Random r;
+	
+	// Constructor
 	public TestplayModule()
 	{
-		// Def
-		//variables=new Object[1][1];
+		r=new Random(System.currentTimeMillis());
 		playerStack=new Stack<Integer>();
 		cardStack=new Stack<Node>();
 		logging=false;
+		// 1. Definition을 로드한다. 또는 인자로 들고온다.
+		
+		// 2. Definition에 따라 variable index dictionary를 구성하고 variables를 초기화한다.
+		
+		// 3. 플레이어 명수에 따라 player_seats를 초기화한다.
 	}
 	
 	// ***
@@ -59,36 +69,53 @@ public class TestplayModule {
 	// Node 형태로 쓰인 언어를 interpret한다.
 	
 	// TODO: Implement this.
+	
+	// 액션 규칙을 수행한다.
 	public void action(Node n)
 	{
 	}
+	
+	// 플레이어의 목록을 계산한다.
 	public ArrayList<Integer> players(Node n)
 	{
 		return null;
 	}
+	
+	// Deck을 계산한다.
 	public Node deck(Node n)
 	{
 		return null;
 	}
-	public Node[] cards(Node n)
+	
+	// 카드의 목록을 계산한다.
+	public ArrayList<Node> cards(Node n)
 	{
 		return null;
 	}
+	
+	// 조건을 계산한다.
 	public boolean cond(Node n)
 	{
 		return true;
 	}
+	
+	// 수치를 계산한다.
 	public int num(Node n)
 	{
 		return 0;
 	}
+	
+	// 문자를 계산한다.
 	public String str(Node n)
 	{
 		return null;
 	}
 	
 	
-	//////////////////////// Actions ////////////////////////
+	///////////////////////// ACTION EVALUATION FUNCTIONS ///////////////////////
+	// Node가 지정하는 액션을 수행한다.
+	// 리턴 : void
+	
 	// card_raw가 지정하는 카드를 deck_raw로 움직인다.
 	public void action_move(Node card_raw, Node deck_raw)
 	{
@@ -111,7 +138,6 @@ public class TestplayModule {
 	public void action_shuffle(Node deck_raw)
 	{
 		Node deck_=deck(deck_raw);
-		Random r=new Random(System.currentTimeMillis());
 		Node temp;
 		int loop, randloc;
 		int numCards=deck_.numChildren();
@@ -127,11 +153,11 @@ public class TestplayModule {
 		}
 	}
 	
-	// crit이 지정하는 조건에 따라 deck_raw가 지정하는 덱의 카드를 섞는다.
+	// crit이 지정하는 조건에 따라 deck_raw가 지정하는 덱의 카드를 정렬한다.
 	public void action_order(Node deck_raw, final Node[] crit)
 	{
 		Node d=deck(deck_raw);
-		// TODO: 의심가는 부분.
+		// TODO: 의심가는 부분. deck을 받아와서 정렬하면 원래 variable에 들어있던 node가 정렬되는가.
 		Collections.sort(d.getAllNode(), new Comparator<Node>() {
 		    public int compare(Node a, Node b) {
 		    	int loop;
@@ -164,8 +190,10 @@ public class TestplayModule {
 	{
 		ArrayList<Integer> players_=players(players_raw);
 		int loop;
+		// 매 플레이어마다 반복
 		for(loop=0;loop<players_.size();loop++)
 		{
+			// 플레이어를 stack에 넣고 액션을 수행한 다음 뺀다.
 			playerStack.push(players_.get(loop));
 			action(action_raw);
 			playerStack.pop();
@@ -223,7 +251,7 @@ public class TestplayModule {
 	}
 	public void select_action(String[] options, Node[] actions)
 	{
-		// TODO: Ask action selection
+		// TODO: UI를 띄워 선택을 요구한다.
 	}
 	
 	
@@ -254,11 +282,12 @@ public class TestplayModule {
 		}
 		return a;
 	}
-	// player_all : 현재 플레이어를 돌려준다.
+	
+	// player_all : 현재 플레이어, 즉 stack의 맨 위에 있는 플레이어를 돌려준다.
 	public ArrayList<Integer> player_current()
 	{
 		ArrayList<Integer> l=new ArrayList<Integer>();
-		l.add(playerStack.lastElement());
+		l.add(playerStack.peek());
 		return l;
 	}
 	
@@ -300,8 +329,10 @@ public class TestplayModule {
 		{
 			index=player_seats.indexOf(p.get(loop));
 			player2insert=player_seats.get(player_index_arrange(index-n));
+			// 왼쪽 n번째 사람
 			if(result.contains(player2insert))
 				result.add(player2insert);
+			// all이 참인 경우: 왼쪽 n-1번째부터 1번째까지의 사람
 			if (all)
 				for(loop2=index-1;loop2>0;loop2--)
 					result.add(player_seats.get(player_index_arrange(index-loop2)));
@@ -315,11 +346,16 @@ public class TestplayModule {
 		int n = num(n_raw);
 		ArrayList<Integer> result = new ArrayList<Integer>();
 		int loop,loop2,index;
+		Integer player2insert;
 		for(loop=0;loop<p.size();loop++)
 		{
 			index=player_seats.indexOf(p.get(loop));
-			result.add(player_seats.get(player_index_arrange(index+n)));
-			if(all)
+			player2insert=player_seats.get(player_index_arrange(index+n));
+			// 오른쪽 n번째 사람
+			if(result.contains(player2insert))
+				result.add(player2insert);
+			// all이 참인 경우: 오른쪽 n-1번째부터 1번째까지의 사람
+			if (all)
 				for(loop2=index-1;loop2>0;loop2--)
 					result.add(player_seats.get(player_index_arrange(index+loop2)));
 		}
@@ -334,9 +370,12 @@ public class TestplayModule {
 		boolean b;
 		for(loop=0;loop<p.size();loop++)
 		{
+			// player를 stack에 올려 조건을 구한다.
 			playerStack.push(p.get(loop));
 			b=cond(condition_raw);
 			playerStack.pop();
+			
+			// 조건이 거짓이면 없앤다.
 			if (!b)
 			{
 				p.remove(loop);
@@ -379,13 +418,115 @@ public class TestplayModule {
 	}
 	public ArrayList<Integer> player_owner(Node[] cards)
 	{
+		// TODO: 구현 안됨
+		return null;
 	}
 	
 	public ArrayList<Integer> select_player(int n, Node players_raw)
 	{
+		// TODO: UI를 띄워 선택을 요구한다.
+		return null;
 	}
 	
-	public Node deck_get(name:String, player:Integer)
+
+	
+	
+	///////////////////////// DECK EVALUATION FUNCTIONS ///////////////////////
+	// Node를 계산하여 덱을 돌려준다.
+	// 리턴 : Node
+	
+	// 특정 플레이어의 덱
+	public Node deck_get(String name, Node player_raw)
+	{
+		// 플레이어를 계산한다.
+		ArrayList<Integer> p = players(player_raw);
+		// TODO: 1명이 아니면 에러를 띄운다.
+		// 플레이어의 덱을 불러온다.
+		return (Node)variables[p.get(0)][player_variable_index.get(name)];
+	}
+	
+	// 이미 정의된 덱
+	public Node deck_predefined(String name)
+	{
+		// 전역 변수에 있으면 불러온다.
+		Integer index = global_variable_index.get(name);
+		if (index!=null)
+			return (Node)variables[0][index];
+		// 플레이어 변수에 있으면 현재 플레이어 기준으로 불러온다.
+		index = player_variable_index.get(name);
+		if (index!=null)
+			return (Node)variables[playerStack.peek()][index];
+		// TODO: 변수 검색 실패. 에러 처리 필요.
+		return null;
+	}
+	
+	// 여러 덱 중 선택
+	public Node select_deck(Node[] decks)
+	{
+		// TODO: UI를 띄워 선택을 요구한다.
+		return null;
+	}
+	
+	///////////////////////// CARD EVALUATION FUNCTIONS ///////////////////////
+	// Node를 계산하여 카드 목록을 돌려준다.
+	// 리턴 : ArrayList<Node>
+	
+	// 특정 덱의 카드
+	public ArrayList<Node> cards_alldeck(Node deck_raw)
+	{
+		return deck(deck_raw).getAllNode();
+	}
+	
+	// 덱 위쪽 n장
+	public ArrayList<Node> cards_top(Node n_raw, Node deck_raw)
+	{
+		int n=num(n_raw);
+		ArrayList<Node> total=deck(deck_raw).getAllNode();
+		// TODO: 덱이 n장보다 크면 오류 처리
+		return (ArrayList<Node>) total.subList(total.size()-n, total.size());
+	}
+	// 덱 아래쪽 n장
+	public ArrayList<Node> cards_bottom(Node n_raw, Node deck_raw)
+	{
+		int n=num(n_raw);
+		ArrayList<Node> total=deck(deck_raw).getAllNode();
+		// TODO: 덱이 n장보다 크면 오류 처리
+		return (ArrayList<Node>) total.subList(0, n);
+	}
+	// 덱 랜덤 n장
+	public ArrayList<Node> cards_random(Node n_raw, Node deck_raw)
+	{
+		int n=num(n_raw);
+		ArrayList<Node> total=deck(deck_raw).getAllNode();
+		// TODO: 덱이 n장보다 크면 오류 처리
+		return (ArrayList<Node>) total.subList(0, n);
+	}
+	
+	public ArrayList<Node> cards_satisfy(Node condition_raw, Node cards_raw)
+	{
+		ArrayList<Node> c = cards(cards_raw);
+		ArrayList<Node> result=new ArrayList<Node>();
+		boolean b;
+		int loop;
+		for(loop=0;loop<c.size();loop++)
+		{
+			cardStack.push(c.get(loop));
+			b=cond(condition_raw);
+			cardStack.pop();
+			if(b)
+			{
+				result.add(c.get(loop));
+				c.remove(loop);
+				loop--;
+			}
+		}
+		return result;
+	}
+	public ArrayList<Node> select_card(Node n_raw, Node deck)
+	{
+		// TODO: UI를 띄워 선택을 요구한다.
+		return null;
+	}
 	
 	
 }
