8daaa869bf312d97693bacbd85824f661c81ff18
Samchon <ysgenius88@gmail.com>
2013-11-22 01:59:24 +0900

    add new project testUI

---
+++ testUI/src/PEBBLET/mainUI.java
@@ -0,0 +1,49 @@
+package PEBBLET;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+import javax.swing.*;
+
+import com.sun.tools.javah.MainDoclet;
+
+
+public class mainUI extends JFrame{
+	public mainUI(){
+		
+		JMenuBar menubar= new JMenuBar();
+		//ImageIcon exit_icon = new ImageIcon(getClass().getResource("exit.png")); 
+		
+		JMenu file = new JMenu("File");
+		file.setMnemonic(KeyEvent.VK_F);
+		
+		JMenuItem eMenuItem= new JMenuItem("Exit");
+		eMenuItem.setMnemonic(KeyEvent.VK_E);
+	    eMenuItem.setToolTipText("Exit application");
+        eMenuItem.addActionListener(new ActionListener() {
+        	@Override
+        	public void actionPerformed(ActionEvent event) {
+                System.exit(0);
+            }
+
+
+        });
+		
+		
+		setTitle("PEBBLET");
+		setSize(1400,1024);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args){
+		SwingUtilities.invokeLater(new Runnable(){
+			@Override
+			public void run(){
+				mainUI ex = new mainUI();
+				ex.setVisible(true);
+			}
+		});
+	}
+}
