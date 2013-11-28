2580c56b1952fd04317042f982306d21787dc41c
Samchon <ysgenius88@gmail.com>
2013-11-29 01:23:13 +0900

    add new class

---
+++ testUI/src/PEBBLET/custominputbox.java
@@ -0,0 +1,45 @@
+package PEBBLET;
+
+import javax.swing.BorderFactory;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
+
+public class custominputbox extends JComponent{	
+	
+	private JComboBox<String> name = new JComboBox<String>();
+	private JTextField detail = new JTextField();
+	
+	public custominputbox(){
+		JPanel inputbox = new JPanel();
+		inputbox.setLayout(null);
+		inputbox.setSize(80,30);
+		Border raisedetched = BorderFactory.createEtchedBorder(EtchedBorder.RAISED);
+        Border loweredbevel = BorderFactory.createLoweredBevelBorder();
+        inputbox.setBorder(BorderFactory.createCompoundBorder(raisedetched, loweredbevel));
+//		JComboBox name = new JComboBox();
+		name.setBounds(5,5,30,20);
+//		JTextField detail = new JTextField();
+		detail.setBounds(40, 5, 35, 20);
+		
+		inputbox.add(name);
+		inputbox.add(detail);
+		
+	}
+	public void additem(custominputbox inputbox, String item_name){
+		JComboBox<String> name = inputbox.getname();
+		name.addItem(item_name);
+	}
+	
+	public JComboBox<String> getname(){
+		return name;
+	}
+	
+	public String getdetail(){
+		return detail.getText();
+	}
+	
+}
