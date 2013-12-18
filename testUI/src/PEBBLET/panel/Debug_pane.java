551d16236c4dc93dd55877f44d83039517e74a46
Samchon <ysgenius88@gmail.com>
2013-12-18 22:15:37 +0900

    fix some bug in debug_pane

--- testUI/src/PEBBLET/panel/Debug_pane.java
+++ testUI/src/PEBBLET/panel/Debug_pane.java
@@ -24,6 +24,7 @@ public class Debug_pane extends JComponent{
 	private boolean completed = false;
 	private JButton start_testplay;
 	private JPanel debug_window_pane;
+	private JScrollPane dbg_window_sc;
 	
 	private int msg_pos = 20;
 	
@@ -40,15 +41,23 @@ public class Debug_pane extends JComponent{
 		Border raisedetched = BorderFactory.createEtchedBorder(EtchedBorder.RAISED);
         Border loweredbevel = BorderFactory.createLoweredBevelBorder();
 		debug_window_pane = new JPanel();
-		debug_window_pane.setBorder(BorderFactory.createCompoundBorder(raisedetched, loweredbevel));
-		debug_window_pane.setBounds(50, 50, 500, 300);
+//		debug_window_pane.setBorder(BorderFactory.createCompoundBorder(raisedetched, loweredbevel));
+		debug_window_pane.setBounds(0, 0, 500, 300);
+		debug_window_pane.setPreferredSize(new Dimension(500,300) );	
+		
+		dbg_window_sc  = new JScrollPane(debug_window_pane);
+		dbg_window_sc.setBorder(BorderFactory.createCompoundBorder(raisedetched, loweredbevel));
+		dbg_window_sc.setPreferredSize(new Dimension(600,350));
+		dbg_window_sc.setSize(600, 350);
+		dbg_window_sc.setLocation(50, 50);
 		
 		start = new JButton("Start Debugging");
 		start_testplay = new JButton("Start TestPlay");
 		start.setBounds(5, 400, 200,30);
 		start_testplay.setBounds(300,400, 200, 30);
 		debug_pane.setLayout(null);
-		debug_pane.add(debug_window_pane);
+//		debug_pane.add(debug_window_pane);
+		debug_pane.add(dbg_window_sc);
 		debug_pane.add(start);
 		debug_pane.add(start_testplay);
 		
@@ -70,8 +79,15 @@ public class Debug_pane extends JComponent{
 		Border raisedetched = BorderFactory.createEtchedBorder(EtchedBorder.RAISED);
         Border loweredbevel = BorderFactory.createLoweredBevelBorder();
 		debug_window_pane = new JPanel();
-		debug_window_pane.setBorder(BorderFactory.createCompoundBorder(raisedetched, loweredbevel));
-		debug_window_pane.setBounds(50, 50, 500, 300);
+//		debug_window_pane.setBorder(BorderFactory.createCompoundBorder(raisedetched, loweredbevel));
+		debug_window_pane.setBounds(0, 0, 500, 300);
+		debug_window_pane.setPreferredSize(new Dimension(500,300) );	
+		
+		dbg_window_sc  = new JScrollPane(debug_window_pane);
+		dbg_window_sc.setBorder(BorderFactory.createCompoundBorder(raisedetched, loweredbevel));
+		dbg_window_sc.setPreferredSize(new Dimension(600,350));
+		dbg_window_sc.setLocation(50, 50);
+		dbg_window_sc.setSize(600, 350);
 		
 		start = new JButton("Start Debugging");
 		start.addActionListener(new ActionListener() {
@@ -94,10 +110,12 @@ public class Debug_pane extends JComponent{
 			}
 		});
 		start_testplay = new JButton("Start TestPlay");
+		
 		start.setBounds(5, 400, 200,30);
 		start_testplay.setBounds(300,400, 200, 30);
 		debug_pane.setLayout(null);
-		debug_pane.add(debug_window_pane);
+//		debug_pane.add(debug_window_pane);
+		debug_pane.add(dbg_window_sc);
 		debug_pane.add(start);
 		debug_pane.add(start_testplay);
 		
@@ -111,8 +129,11 @@ public class Debug_pane extends JComponent{
 //		msg.setLocation(5, msg_pos);
 		msg.setBounds(5, msg_pos, msg_.length()*15,30);
 		msg_pos+=30;
+		debug_window_pane.setPreferredSize(new Dimension(450, msg_pos+20));
 		debug_window_pane.setLayout(null);
 		debug_window_pane.add(msg);
+		repaint();
+		validate();
 	}
 	
 	public JComponent get_scpane(){
